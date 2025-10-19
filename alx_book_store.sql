b67a2a3049321f00e1d558b3b0a21a2eeaac70e7
Kiplagat Keith <kiplagatkeith@proton.me>
2025-10-19 20:01:01 +0300

    Into to db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,6 +38,7 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,

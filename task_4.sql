ecaf3fabd468796e2de63535a9cde9285f5c64ee
Kiplagat Keith <kiplagatkeith@proton.me>
2025-10-19 21:52:44 +0300

    Intro to db

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';

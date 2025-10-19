a05447acfa9d79f97751cc9f8d700241fc2d4bf7
Kiplagat Keith <kiplagatkeith@proton.me>
2025-10-19 19:40:23 +0300

    New to db

---
+++ alx_book_store.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name varchar(255),
+    email VARCHAR(255),
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    order_date DATE
+);
